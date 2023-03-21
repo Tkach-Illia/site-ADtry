9fd7b0c3d46f8839bcafae3ba577d181c204ff00
Tkach-Illia <tkachillia00@gmail.com>
2023-03-21 16:47:07 +0200

    style: stylize code

--- components/Fetcher.tsx
+++ components/Fetcher.tsx
@@ -1,12 +1,12 @@
-import useSWR from 'swr';
+import useSWR from "swr";
 import Image from "next/image";
 import axios from "axios";
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from "react";
 
 interface Cell {
-    value: number;
-    symbol: string;
-  }
+  value: number;
+  symbol: string;
+}
 
 const fetcher = (url: string) => fetch(url).then((res) => res.json());
 
@@ -15,21 +15,30 @@ function Profile() {
     Array.from(Array(5), () => new Array(5).fill({ value: 0, symbol: "w" }))
   );
 
-  const { data, error } = useSWR(`/api/hello`, fetcher, { refreshInterval: 500 });
-  
-  useEffect(()=>{
-    if(data){
+  const { data, error } = useSWR(`/api/hello`, fetcher, {
+    refreshInterval: 500,
+  });
+
+  useEffect(() => {
+    if (data) {
       setGrid(data.grid);
     }
-  },[data]);
-  
+  }, [data]);
 
   if (error) return <div>Failed to load user</div>;
   if (!data) return <div>Loading...</div>;
 
-  const updateUser = async (rowIndex: number, colIndex: number, symbol: string) => {
+  const updateUser = async (
+    rowIndex: number,
+    colIndex: number,
+    symbol: string
+  ) => {
     try {
-      const response = await axios.put(`/api/hello`, { rowIndex: rowIndex, colIndex: colIndex, symbol: symbol });
+      const response = await axios.put(`/api/hello`, {
+        rowIndex: rowIndex,
+        colIndex: colIndex,
+        symbol: symbol,
+      });
       return response.data;
     } catch (error) {
       console.error(error);
@@ -37,7 +46,7 @@ function Profile() {
   };
 
   const handleCellClick = async (rowIndex: number, colIndex: number) => {
-    const updatedData = await updateUser(rowIndex, colIndex, 'g');
+    const updatedData = await updateUser(rowIndex, colIndex, "g");
     setGrid(updatedData.grid);
   };
   const handleContextMenu = async (
@@ -45,52 +54,51 @@ function Profile() {
     rowIndex: number,
     colIndex: number
   ) => {
-    
-    const updatedData = await updateUser(rowIndex, colIndex, 'r');
+    const updatedData = await updateUser(rowIndex, colIndex, "r");
     setGrid(updatedData.grid);
     event.preventDefault();
   };
   return (
     <div>
-      {Array.isArray(grid) && grid.map((row, rowIndex) => (
-        <div key={rowIndex} style={{ display: "flex" }}>
-          {row.map((cell, colIndex) => (
-            <div
-              key={`${rowIndex}-${colIndex}`}
-              style={{
-                width: "50px",
-                height: "50px",
-                backgroundColor: "white",
-                border: "1px solid black",
-                display: "flex",
-                flexDirection: "column",
-                alignItems: "center",
-                justifyContent: "center",
-                userSelect: "none",
-              }}
-              onClick={() => handleCellClick(rowIndex, colIndex)}
-              onContextMenu={(event) =>
-                handleContextMenu(event, rowIndex, colIndex)
-              }
-            >
-              {cell.value <= 3 ? (
-                <Image
-                  src={`/${cell.symbol}/number-${cell.value}.png`}
-                  alt={`${cell.value}`}
-                  width={30}
-                  height={30}
-                />
-              ) : (
-                <div style={{ fontSize: "20px" }}>{cell.value}</div>
-              )}
-            </div>
-          ))}
-        </div>
-      ))}
+      {Array.isArray(grid) &&
+        grid.map((row, rowIndex) => (
+          <div key={rowIndex} style={{ display: "flex" }}>
+            {row.map((cell, colIndex) => (
+              <div
+                key={`${rowIndex}-${colIndex}`}
+                style={{
+                  width: "50px",
+                  height: "50px",
+                  backgroundColor: "white",
+                  border: "1px solid black",
+                  display: "flex",
+                  flexDirection: "column",
+                  alignItems: "center",
+                  justifyContent: "center",
+                  userSelect: "none",
+                }}
+                onClick={() => handleCellClick(rowIndex, colIndex)}
+                onContextMenu={(event) =>
+                  handleContextMenu(event, rowIndex, colIndex)
+                }
+              >
+                {cell.value <= 3 ? (
+                  <Image
+                    src={`/${cell.symbol}/number-${cell.value}.png`}
+                    alt={`${cell.value}`}
+                    width={30}
+                    height={30}
+                  />
+                ) : (
+                  <div style={{ fontSize: "20px" }}>{cell.value}</div>
+                )}
+              </div>
+            ))}
+          </div>
+        ))}
     </div>
   );
-};
-
+}
 
 export default function Home1() {
   return <Profile />;
--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -9,46 +9,45 @@ interface Cell {
   symbol: string;
 }
 
-let arr = Array.from(
-  Array(5),
-  () => new Array(5).fill({ value: 0, symbol: "w" })
+let arr = Array.from(Array(5), () =>
+  new Array(5).fill({ value: 0, symbol: "w" })
 );
 
-function updateArr(){
+function updateArr() {
   const newGrid = [...arr];
-    for (let rowIndex = 0; rowIndex < 5; rowIndex++)
-      for (let colIndex = 0; colIndex < 5; colIndex++)
-        if (arr[rowIndex][colIndex].value >= 4) {
-          newGrid[rowIndex][colIndex] = {
+  for (let rowIndex = 0; rowIndex < 5; rowIndex++)
+    for (let colIndex = 0; colIndex < 5; colIndex++)
+      if (arr[rowIndex][colIndex].value >= 4) {
+        newGrid[rowIndex][colIndex] = {
+          symbol: arr[rowIndex][colIndex].symbol,
+          value: newGrid[rowIndex][colIndex].value - 4,
+        };
+        if (rowIndex > 0) {
+          newGrid[rowIndex - 1][colIndex] = {
             symbol: arr[rowIndex][colIndex].symbol,
-            value: newGrid[rowIndex][colIndex].value - 4,
-          };
-          if (rowIndex > 0) {
-            newGrid[rowIndex - 1][colIndex] = {
-              symbol: arr[rowIndex][colIndex].symbol,
-              value: newGrid[rowIndex - 1][colIndex].value + 1,
-            }; // Вгорі
-          }
-          if (rowIndex < 4) {
-            newGrid[rowIndex + 1][colIndex] = {
-              symbol: arr[rowIndex][colIndex].symbol,
-              value: newGrid[rowIndex + 1][colIndex].value + 1,
-            }; // Знизу
-          }
-          if (colIndex > 0) {
-            newGrid[rowIndex][colIndex - 1] = {
-              symbol: arr[rowIndex][colIndex].symbol,
-              value: newGrid[rowIndex][colIndex - 1].value + 1,
-            }; // Зліва
-          }
-          if (colIndex < 4) {
-            newGrid[rowIndex][colIndex + 1] = {
-              symbol: arr[rowIndex][colIndex].symbol,
-              value: newGrid[rowIndex][colIndex + 1].value + 1,
-            }; // Справа
-          }
+            value: newGrid[rowIndex - 1][colIndex].value + 1,
+          }; // Вгорі
         }
-        arr = newGrid;
+        if (rowIndex < 4) {
+          newGrid[rowIndex + 1][colIndex] = {
+            symbol: arr[rowIndex][colIndex].symbol,
+            value: newGrid[rowIndex + 1][colIndex].value + 1,
+          }; // Знизу
+        }
+        if (colIndex > 0) {
+          newGrid[rowIndex][colIndex - 1] = {
+            symbol: arr[rowIndex][colIndex].symbol,
+            value: newGrid[rowIndex][colIndex - 1].value + 1,
+          }; // Зліва
+        }
+        if (colIndex < 4) {
+          newGrid[rowIndex][colIndex + 1] = {
+            symbol: arr[rowIndex][colIndex].symbol,
+            value: newGrid[rowIndex][colIndex + 1].value + 1,
+          }; // Справа
+        }
+      }
+  arr = newGrid;
 }
 
 export default function handler(
